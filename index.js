0e419449c8ed47d9c22f549cdb46dedbdcfe1718
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-05-06 20:13:18 -0700

    commit tree 85cb2c9008a8f7308fa7e5b18779b8d7be851794

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 // Write your solution in this file!
 const employee = {}
-    employee.name = 'Sam'
+    x.name = 'Sam'
     streetAddress = '11 Broadway'
+    // employee.name = 'Sam'
+    // streetAddress = '11 Broadway'
+    // To show that I know what I am talking about, I changed the arbitrary value to x 
+    // because we don't necessarily need the name, we just need to know what 
+    // the function is doing. Here we are creating an object. 
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key]=value
-    return employee;
+function destructivelyUpdateEmployeeWithKeyAndValue(why, key, value){
+    why[key]=value
+    return why;
+    // employee[key]=value
+    // return employee; 
+    // When changing the arbitrary value, it is important to make sure, JS knows 
+    // what you are talking about. Otherwise, when you use an arbitrary value and 
+    // are trying to callback the function, it won't work. 
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newObj={...employee};
-    newObj[key]=value;
+function updateEmployeeWithKeyAndValue(redo, key, value){
+    const newObj={...redo};
+    newObj [key]=value;
     return newObj;
+    // const newObj={...employee};
+    // newObj[key]=value;
+    // return newObj;
+    // With this one, I am defining the object so that way I am able to use it 
+    // within the function. This function is destructively updating 'data' 
+    // by destroyinh the object aqnd creating a new one. 
+
 }
 
-function deleteFromEmployeeByKey(employee, key){
-    const newObj={...employee}
+function deleteFromEmployeeByKey(the, key){
+    const newObj={...the}
     delete newObj[key]
-    return newObj;
+    return newObj
+    // const newObj={...employee}
+    // delete newObj[key]
+    // return newObj;
+    // Here we are non-destructively deleteing key. How this is done 
+    // is by not mutating the object itself. 
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key){
+function destructivelyDeleteFromEmployeeByKey(lab, key){
     delete employee[key]
-    return employee
+    return lab
+    // delete employee[key]
+    // return employee
+    // 
 }
